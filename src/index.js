e76409d2caf412fc3ed0c52aa6ae520da05df74b
rajeshmanjunath <rajesh@thisisalice.com>
2022-02-01 16:32:20 +0530

    sass/scss

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/styles.css';
+import './styles/styles.scss';
 
 // without babel preset added it will work
 // ReactDOM.render(

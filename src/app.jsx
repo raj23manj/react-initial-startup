f55d6c3c518ea8d582cb8d4647357ff8199b6c38
rajeshmanjunath <rajesh@thisisalice.com>
2022-02-01 18:39:03 +0530

    using iframe like plugin

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,48 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [showModal, setshowModal] = useState(false)
+  let showHideClass = null;
+
   lifecycles();
 
   function clidkedHandler() {
+    setshowModal(!showModal);
     console.log("clicked parent!!!");
     const iframeEle = document.getElementsByClassName("iframe-content")[0];
     // debugger;
     iframeEle.contentWindow.postMessage("hi from parent!", '*');
   };
 
-  return(
+  function closePopUp(){
+    setshowModal(!showModal);
+  }
+
+  if(showModal) {
+    showHideClass = "modal";
+  } else {
+    showHideClass = "hide modal";
+  }
+
+  return (
     <React.Fragment>
       <div>
         <button onClick={clidkedHandler}>click me</button>
       </div>
-      <div>
+      {/* <div>
         <iframe src='http://quickbuy-browser.robox.trade/app/investor/quickbuy/login'
                 className={"iframe-content"}>
         </iframe>
+      </div> */}
+      <div id="react-app" className={showHideClass}>
+        <div className="modal-content">
+          <div>
+            <span className="close" onClick={closePopUp} id="close-robox">&times;</span>
+          </div>
+          <div id="robox_react">
+            <iframe src="http://quickbuy-browser.robox.trade/app/investor/quickbuy/login" className={"iframe-content"}></iframe>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
@@ -48,6 +72,7 @@ const Root = () => {
       if (e.origin === "http://localhost:8001") {
         console.log("data", e);
         console.log("data", e.data);
+        localStorage.setItem('parent-data', e.data);
 
       }
 
